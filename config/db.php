d1c322fadfda3fbd2d767b6678494d257fb1822c
mehwish0711 <mehwishweb123@gmail.com>
2024-12-20 18:40:04 +0500

    Initial project setup

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+try {
+    $dsn = "mysql:host=localhost;dbname=ecommerce_db";
+    $username = "root";
+    $password = "";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+    $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
